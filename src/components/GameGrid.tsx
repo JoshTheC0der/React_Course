ea2bdf831b4ed312f6d9f03bca8df3c047f30501
Josh <joshuamj71@gmail.com>
2024-10-17 17:09:15 +0100

    Show loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,15 @@
-import { SimpleGrid, Text } from '@chakra-ui/react';
+import { SimpleGrid, Skeleton, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
+import GameCardSkeleton from './GameCardSkeleton';
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  // isLoading is a bool designed by Chakra to add animation/functionality to the loading elements
+  // switch it on (true) whilst loading but off once done (false)
+
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
+
   return (
     <>
       {error && <Text>{error}</Text>}
@@ -12,6 +18,10 @@ const GameGrid = () => {
         spacing={10}
         padding="10px"
       >
+        {/* inner .map('skeleton') is arbitrary and could be any string e.g. .map(beeswax)*/}
+        {/* it represents each element inside the skeletons array */}
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
