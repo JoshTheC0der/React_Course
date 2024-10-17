056a051537a6d63035ac91b1480406730f117177
Josh <joshuamj71@gmail.com>
2024-10-17 16:24:48 +0100

    Get optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,9 @@
+const getCroppedImageUrl = (url: string) => {
+  const target = 'media/';
+  // as indexOf will find the index of the first letter but we want the index of the last symbol so
+  // as to start the splicing from after 'media/'
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+};
+
+export default getCroppedImageUrl;
